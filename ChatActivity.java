9a41f3b63f8c34379cb63f075d830eb178239e8a
Nouha-Hanine <153952572+Nouha-Hanine@users.noreply.github.com>
2024-02-09 13:18:21 +0000

    Update ChatActivity.java

--- ChatActivity.java
+++ ChatActivity.java
@@ -1,29 +1,32 @@
-package com.example.saqsi;
+package edu.cmu.pocketsphinx.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import edu.cmu.pocketsphinx.Hypothesis;
-import edu.cmu.pocketsphinx.SpeechRecognizer;
-import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.net.Uri;
-
 import android.os.Bundle;
-import android.widget.Button;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class ChatActivity extends AppCompatActivity implements VoiceRecognitionManager.RecognitionListener {
+import edu.cmu.pocketsphinx.Hypothesis;
 
-
+public class ChatActivity extends AppCompatActivity {
+    //note we have to add some lines on android manifest si android = 11
+    private TextToSpeech textToSpeech;
     private VoiceRecognitionManager voiceRecognitionManager;
     private Button startListeningButton;
     private boolean isRecording = false;
+    Button logoutButton;
 
     private RecognitionListener recognitionListener;
     private DatabaseHelper dbHelper;
@@ -31,39 +34,33 @@ public class ChatActivity extends AppCompatActivity implements VoiceRecognitionM
     public void setRecognitionListener(RecognitionListener listener) {
         this.recognitionListener = listener;
     }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        logoutButton=(Button) findViewById(R.id.logoutButton);
         startListeningButton = findViewById(R.id.startRecordingButton);
         dbHelper = new DatabaseHelper(this);
         voiceRecognitionManager = new VoiceRecognitionManager(this);
-
-        startListeningButton.setOnClickListener(new View.OnClickListener() {
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
-            public void onClick(View v) {
-                if (isRecording) {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = textToSpeech.setLanguage(Locale.US);
 
-                    isRecording = false;
-                    voiceRecognitionManager.stopListening();
+                    if (  result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA) {
+                        Log.e("tts", "language data is missing of langauge not supported");
+                            }else{
+                        Log.i("tts", "lang supported and data is NOT missing");//TODO fix this
+                    }
                 } else {
-
-                    isRecording = true;
-                    voiceRecognitionManager.startListening();
+                    Log.e("tts", "tts not init");
                 }
             }
         });
-
-
-
-
-
-
         voiceRecognitionManager.setRecognitionListener(new VoiceRecognitionManager.RecognitionListener() {
             @Override
-            public void onResult(String hypothesis) {
+            public void onResult(Hypothesis hypothesis) {
                 if (hypothesis != null) {
                     String spokenText = hypothesis.getHypstr();
                     if (isRecording) {
@@ -78,8 +75,41 @@ public class ChatActivity extends AppCompatActivity implements VoiceRecognitionM
 
 
         });
+        startListeningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRecording) {
+
+                    isRecording = false;
+                    voiceRecognitionManager.stopListening();
+                } else {
+
+                    isRecording = true;
+                    voiceRecognitionManager.startListening();
+                }
+            }
+        });
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                        db.close();
+                SharedPreferences sharedPref = getSharedPreferences("preference", Context.MODE_PRIVATE); //to supp all login data
+                sharedPref.edit().clear().commit();
+
+                Intent intent = new Intent(ChatActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 
 
+    private void speakText( String text) {
+        //TODO to fix later par rapp au texte param DONE ?
+        if(text!=null) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
     }
 
     @Override
@@ -87,33 +117,35 @@ public class ChatActivity extends AppCompatActivity implements VoiceRecognitionM
         if (voiceRecognitionManager != null) {
             voiceRecognitionManager.stopListening();
         }
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
         super.onDestroy();
     }
-
-
     private void processVoiceCommand(String command) throws IOException {
         if (command.contains("joke")) {
             String joke = dbHelper.getJoke();
 
             if (joke != null) {
-                voiceRecognitionManager.speak(joke);
+               speakText(joke);
             } else {
 
-                voiceRecognitionManager.speak("I'm sorry, I couldn't find a joke.");
+               speakText("I'm sorry, I couldn't find a joke.");
             }
         } else if (command.contains("study")) {
             String studyInfo = dbHelper.getCurrentStudySubject();
 
             if (studyInfo != null) {
-                voiceRecognitionManager.speak("You have " + studyInfo);
+                speakText("You have " + studyInfo);
             } else {
 
-                voiceRecognitionManager.speak("I'm sorry, I couldn't find your current study information.");
+                speakText("I'm sorry, I couldn't find your current study information.");
             }
         } else if (command.contains("time")) {
             String currentTime = getCurrentTime();
+            speakText("The current time is " + currentTime);
 
-            voiceRecognitionManager.speak("The current time is " + currentTime);
         } else if (command.contains("music")) {
             playMusic();
         }
